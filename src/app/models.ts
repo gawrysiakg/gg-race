1ca3309bdb3485a5e4e7345ad8d49de1306138dd
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2024-01-29 20:13:46 +0100

    list font changed

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,33 +1,31 @@
 export interface User {
-    id: number;
-    name: string;
-    email: string;
-    points: number;
-    lastLoggedIn: Date;
-    lastGameHistory: Array <GameHistory>
-  }
+  id: number;
+  name: string;
+  email: string;
+  points: number;
+  lastLoggedIn: Date;
+  lastGameHistory: Array<GameHistory>;
+}
 
-  export interface GameHistory {
-    gameStatus: GameStatus,
-    date: Date,
-    elapsedTime: number;
+export interface GameHistory {
+  gameStatus: GameStatus;
+  date: Date;
+  elapsedTime: number;
+}
 
-  }
-
-
-  export enum GameStatus {
-    READY = 'READY',
-    STARTED = 'STARTED',
-    PAUSED = 'PAUSED',
-    FINISHED = 'FINISHED',
-    RESETED = 'RESETED',
-    GAME_OVER = 'GAME OVER',
-    QUIT_GAME = 'QUIT GAME',
-    OVERTAKING = 'OVERTAKING',
-    TURBO_ON = 'TURBO ON',
-    TURBO_OFF = 'TURBO OFF',
-    DARK_MODE_ON = 'DARK MODE ON',
-    DARK_MODE_OFF = 'DARK MODE OFF',
-    SIMPLE_VIEW = 'SIMPLE VIEW',
-    EXTENDED_VIEW = 'EXTENDED VIEW'
-  }
+export enum GameStatus {
+  READY = 'READY',
+  STARTED = 'STARTED',
+  PAUSED = 'PAUSED',
+  FINISHED = 'FINISHED',
+  RESETED = 'RESETED',
+  GAME_OVER = 'GAME OVER',
+  QUIT_GAME = 'QUIT GAME',
+  OVERTAKING = 'OVERTAKING',
+  TURBO_ON = 'TURBO ON',
+  TURBO_OFF = 'TURBO OFF',
+  DARK_MODE_ON = 'DARK MODE',
+  DARK_MODE_OFF = 'LIGHT MODE',
+  SIMPLE_VIEW = 'SIMPLE VIEW',
+  EXTENDED_VIEW = 'EXTENDED VIEW',
+}
