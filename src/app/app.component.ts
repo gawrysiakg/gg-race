9d6a2879f225738398b69b97db9015e4f0d71e25
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2024-03-10 20:49:00 +0100

    intro and game routing init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,74 +4,77 @@ import { GameComponent } from './game/game.component';
 import { ScoreComponent } from './score/score.component';
 import { GameStatus, User } from './models';
 import { PersonFormComponent } from './person-form/person-form.component';
-
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NgFor, GameComponent, ScoreComponent, PersonFormComponent],
+  imports: [
+    CommonModule,
+    NgFor,
+    GameComponent,
+    ScoreComponent,
+    PersonFormComponent,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'gg-race';
 
-  usersList:Array<User> = [];
+  usersList: Array<User> = [];
   public isGameStarted = false;
-  public showScore=false;
-  public scoreButtonText =  "Show score";
-  public userId =1;
+  public showScore = false;
+  public scoreButtonText = 'Show score';
+  public userId = 1;
   public player: User | undefined;
   public playerName: string = '';
-  public isMainScreen=true;
+  public isMainScreen = true;
   public isLoggedIn = false;
 
-  setCurrentPlayer(event: User){
-    this.player=event;
-    this.player.id=this.userId;
+  setCurrentPlayer(event: User) {
+    this.player = event;
+    this.player.id = this.userId;
     this.userId++;
-    this.playerName=this.player.name;
-    this.isLoggedIn=true;
-  }
-  
-  
-  startGame(){
-    this.isGameStarted=true;
-    this.isMainScreen=false;
-    this.player?.lastGameHistory.push({gameStatus: GameStatus.READY, date: new Date(), elapsedTime: 0})
+    this.playerName = this.player.name;
+    this.isLoggedIn = true;
   }
 
+  startGame() {
+    this.isGameStarted = true;
+    this.isMainScreen = false;
+    this.player?.lastGameHistory.push({
+      gameStatus: GameStatus.READY,
+      date: new Date(),
+      elapsedTime: 0,
+    });
+  }
 
-  quitGame(event: boolean){
-    this.isGameStarted=event;  //event z gry jak skończy grać
-    if(this.player){
+  quitGame(event: boolean) {
+    this.isGameStarted = event; //event z gry jak skończy grać
+    if (this.player) {
       const newPlayer: User = { ...this.player };
       this.usersList.push(newPlayer);
-
     }
-    this.isMainScreen=true;
+    this.isMainScreen = true;
   }
 
-
-  logout(){
-    this.player=undefined;
-    this.playerName='';
-    this.isLoggedIn=false;
-    this.showScore=true;
-    this.showScore=false;
+  logout() {
+    this.player = undefined;
+    this.playerName = '';
+    this.isLoggedIn = false;
+    this.showScore = true;
+    this.showScore = false;
   }
 
-
-  toggleScore(){
-    this.scoreButtonText = this.showScore ? "Show score" : "Hide score";
-    this.showScore=!this.showScore;
-    
+  toggleScore() {
+    this.scoreButtonText = this.showScore ? 'Show score' : 'Hide score';
+    this.showScore = !this.showScore;
   }
 
-
-  displayScoreAfterGame(){
+  displayScoreAfterGame() {
     this.toggleScore();
-    this.isGameStarted=false;
+    this.isGameStarted = false;
   }
-
 }
